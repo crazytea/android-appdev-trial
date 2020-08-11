9fb284e82d52c8b502a7f32eb7477449caed22fe
LittleYellow <crazytea@gmail.com>
2020-08-11 10:40:05 +0800

    [Add] 新增 extent browser 功能、畫面優化

--- app/src/main/java/com/yellow/tp_zoo/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/yellow/tp_zoo/ui/dashboard/DashboardFragment.kt
@@ -1,16 +1,21 @@
 package com.yellow.tp_zoo.ui.dashboard
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.yellow.tp_zoo.DataModel.DataZooInfo
 import com.yellow.tp_zoo.R
+
 
 class DashboardFragment : Fragment() {
 
@@ -30,10 +35,17 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        (activity as AppCompatActivity).supportActionBar?.title = ""
         textView_info = root.findViewById(R.id.txt_info_info)
         textView_category = root.findViewById(R.id.txt_info_category)
         textView_memo = root.findViewById(R.id.txt_info_memo)
         imageView_banner = root.findViewById(R.id.img_banner)
+        val button: Button = root.findViewById(R.id.btn_info_extent)
+        button.setOnClickListener {
+            clickExtentOutBrowser()
+        }
+
 //        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
@@ -50,7 +62,20 @@ class DashboardFragment : Fragment() {
         valueZo = zooData
         textView_info?.let { textView_info!!.text = zooData.E_Info}
         textView_category?.let { textView_category!!.text = zooData.E_Category}
-        textView_memo?.let { textView_memo!!.text = zooData.E_Memo}
+        textView_memo?.let {
+            if (zooData.E_Memo == "") { textView_memo!!.text = "(無備註)"}
+            else {textView_memo!!.text = zooData.E_Memo}
+        }
+        Glide.with(context).load(zooData.E_Pic_URL).centerCrop().into(imageView_banner)
+        (activity as AppCompatActivity).supportActionBar?.title = zooData.E_Name
     }
+
+    // 用外部瀏覽器開啟
+    private fun clickExtentOutBrowser() {
+        val browserIntent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(valueZo?.E_URL))
+        startActivity(browserIntent)
+    }
+
 
 }
